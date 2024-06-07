05c0080b0b058958f7f6cb8fe35b4ddff157036a
Maks <maxkov19861411@gmail.com>
2024-06-07 19:11:59 +0300

    Update files

---
+++ src/components/Column/Column.jsx
@@ -0,0 +1,12 @@
+import TasksList from '../TasksList/TasksList';
+
+const Column = () => {
+	return (
+		<>
+			<h1>Column</h1>
+			<TasksList />
+		</>
+	);
+};
+
+export default Column;
---
+++ src/components/ColumnList/ColumnList.jsx
@@ -0,0 +1,12 @@
+import Column from '../Column/Column';
+
+const ColumnList = () => {
+	return (
+		<>
+			<h1>ColumnList</h1>
+			<Column />
+		</>
+	);
+};
+
+export default ColumnList;
