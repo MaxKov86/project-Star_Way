64b790c74bb0191bef122c4b24e2aca274a3f1b2
Maks <maxkov19861411@gmail.com>
2024-06-07 17:33:47 +0300

    Added and updated new folders and files

--- src/components/TasksList/TasksList.jsx
+++ src/components/TasksList/TasksList.jsx
@@ -1,2 +1,10 @@
-const TasksList = () => {};
+import TaskCard from '../TaskCard/TaskCard';
+const TasksList = () => {
+	return (
+		<>
+			<h1>TasksList</h1>
+			<TaskCard />
+		</>
+	);
+};
 export default TasksList;
--- src/pages/ScreensPage/ScreenPage.jsx
+++ src/pages/ScreensPage/ScreenPage.jsx
@@ -1,5 +1,12 @@
+import ColumnList from '../../components/ColumnList/ColumnList';
+
 const ScreenPage = () => {
-	return <h1>ScreenPage</h1>;
+	return (
+		<>
+			<h1>ScreenPage</h1>
+			<ColumnList />
+		</>
+	);
 };
 
 export default ScreenPage;
