210f9c904085a843dc143bebfd688845a8fbcf87
Maks <maxkov19861411@gmail.com>
2024-06-07 13:21:13 +0300

    Updated vite.config.js and added plugin svgr

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    sourcemap: true,
-  },
+	plugins: [react(), svgr()],
+	build: {
+		sourcemap: true,
+	},
+	resolve: {
+		alias: {
+			src: '/src',
+			components: '/src/components',
+			pages: '/src/pages',
+			assets: '/src/assets',
+		},
+	},
+	base: '/project-star-way',
 });
