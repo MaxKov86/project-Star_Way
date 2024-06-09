65134bbb90ac9c9879c6068bd90558713ce8e131
Maks <maxkov19861411@gmail.com>
2024-06-09 11:35:36 +0300

    Updated persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import themeReduser from './theme/slice';
 
@@ -15,6 +24,12 @@ export const store = configureStore({
 	reducer: {
 		theme: persistThemeReduser,
 	},
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
 });
 
 export const persistor = persistStore(store);
