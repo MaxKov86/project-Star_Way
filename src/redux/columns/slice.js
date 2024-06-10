c224f7b837166ff0e8964b0b0b8beb4223bee5db
Maks <maxkov19861411@gmail.com>
2024-06-10 16:27:50 +0300

    Updated column slice

--- src/redux/columns/slice.js
+++ src/redux/columns/slice.js
@@ -6,45 +6,45 @@ const slice = createSlice({
 	initialState: {
 		items: [],
 		error: false,
-		IsLoading: false,
+		isLoading: false,
 	},
 	extraReducers: builder => {
 		builder
 			.addCase(getAllColumns.pending, state => {
 				state.error = false;
-				state.IsLoading = true;
+				state.isLoading = true;
 			})
 			.addCase(getAllColumns.fulfilled, (state, action) => {
 				state.items = action.payload;
-				state.IsLoading = false;
+				state.isLoading = false;
 			})
 			.addCase(getAllColumns.rejected, state => {
 				state.error = true;
-				state.IsLoading = false;
+				state.isLoading = false;
 			})
 			.addCase(createColumn.pending, state => {
 				state.error = false;
-				state.IsLoading = true;
+				state.isLoading = true;
 			})
 			.addCase(createColumn.fulfilled, (state, action) => {
 				state.items.push(action.payload);
-				state.IsLoading = false;
+				state.isLoading = false;
 			})
 			.addCase(createColumn.rejected, state => {
 				state.error = true;
-				state.IsLoading = false;
+				state.isLoading = false;
 			})
 			.addCase(deleteColumn.pending, state => {
 				state.error = false;
-				state.IsLoading = true;
+				state.isLoading = true;
 			})
 			.addCase(deleteColumn.fulfilled, (state, action) => {
 				state.items = state.items.filter(item => item.id !== action.payload.id);
-				state.IsLoading = false;
+				state.isLoading = false;
 			})
 			.addCase(deleteColumn.rejected, state => {
 				state.error = false;
-				state.IsLoading = true;
+				state.isLoading = true;
 			});
 	},
 });
