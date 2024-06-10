97d36351110b6649ab06927cb498651dd327f26c
timatas <139263028+timatas@users.noreply.github.com>
2024-06-10 14:58:41 +0300

    icon-close

--- src/components/ModalWindowHelp/ModalWindowHelp.jsx
+++ src/components/ModalWindowHelp/ModalWindowHelp.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import css from './ModalWindowHelp.module.css';
-import sprite from '../../assets/icons.svg';
 
 const ModalWindowHelp = ({ onClose }) => {
 	const [title, setTitle] = useState('');
@@ -14,8 +13,27 @@ const ModalWindowHelp = ({ onClose }) => {
 	return (
 		<div className={css.modalContainer}>
 			<button className={css.closeButton} onClick={onClose}>
-				<svg id="icon-x-close" viewBox="0 0 32 32">
-					<use href={`${sprite}#icon-x-close`} />
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="18"
+					height="18"
+					viewBox="0 0 18 18"
+					fill="none"
+				>
+					<path
+						d="M13.5 4.5L4.5 13.5"
+						stroke="white"
+						strokeWidth="1.5"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+					/>
+					<path
+						d="M4.5 4.5L13.5 13.5"
+						stroke="white"
+						strokeWidth="1.5"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+					/>
 				</svg>
 			</button>
 			<h2 className={css.title}>Need help</h2>
