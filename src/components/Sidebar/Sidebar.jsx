f331e77a832043e58564d162a5e5f79617e71ed3
timatas <139263028+timatas@users.noreply.github.com>
2024-06-10 19:00:36 +0300

    delete comments in sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,11 +2,6 @@ import LogoComponent from '../LogoComponent/LogoComponent';
 import NeedHelp from './NeedHelp/NeedHelp';
 import css from './Sidebar.module.css';
 
-// const Sidebar = ({ visible, onVisible }) => {
-// 	const closeSidebar = () => {
-// 		onVisible(false);
-// 	};
-
 const Sidebar = () => {
 	return (
 		<div className={css.sidebar}>
@@ -18,14 +13,3 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
-
-//=====
-//  <div className={clsx(css.sidebarWrap, { [css.visible]: visible })}
-// 			onClick={closeSidebar}
-// 		>
-// 			<aside className={css.sidebar}>
-// 				<div className={css.block}>
-// 					<NeedHelp />
-// 				</div>
-// 			</aside>
-// 		</div>
