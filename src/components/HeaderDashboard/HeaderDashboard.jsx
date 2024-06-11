fa47b38177bb2713da3d8966d11cec5bef1387d8
Denys <bakhonin1@gmail.com>
2024-06-11 14:59:42 +0200

    title, filter

--- src/components/HeaderDashboard/HeaderDashboard.jsx
+++ src/components/HeaderDashboard/HeaderDashboard.jsx
@@ -1,3 +1,40 @@
-const HeaderDashbord = () => {};
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { clearFilter, setFilter } from '../../redux/filter/slice.js';
+import clsx from 'clsx';
 
-export default HeaderDashbord;
+import { selectTheme } from '../../redux/theme/selectors';
+import DashboardFilter from '../DashboardFilter/DashboardFilter';
+import css from './HeaderDashboard.module.css';
+
+const HeaderDashboard = () => {
+	const { boardName } = useParams();
+	const [isFilterModalOpen, setIsFilterModalOpen] = useState(false);
+
+	const theme = useSelector(selectTheme);
+	const dispatch = useDispatch();
+
+	const handleFilterChange = filter => {
+		if (filter === null) {
+			dispatch(clearFilter());
+		} else {
+			dispatch(setFilter(filter));
+		}
+	};
+
+	return (
+		<div className={clsx(css.headerDashboardContainer, css[theme])}>
+			<h2 className={clsx(css.titleHeaderDashboard, css[theme])}>
+				{boardName}
+			</h2>
+			<DashboardFilter
+				isFilterModalOpen={isFilterModalOpen}
+				toggleFilterModal={() => setIsFilterModalOpen(!isFilterModalOpen)}
+				onFilterChange={handleFilterChange}
+			/>
+		</div>
+	);
+};
+
+export default HeaderDashboard;
