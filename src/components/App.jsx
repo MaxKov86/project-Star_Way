8c83816518b6ac60efa8a4706c920c6f27ae3ab5
Maks <maxkov19861411@gmail.com>
2024-06-11 18:42:59 +0300

    Remove PrivateRoute

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,6 @@ import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { refreshUser } from '../redux/auth/operations';
-import PrivateRoute from './PrivateRoute';
 
 function App() {
 	const dispatch = useDispatch();
@@ -23,10 +22,7 @@ function App() {
 				<Routes>
 					<Route path="/" element={<WelcomePage />} />
 					<Route path="/auth/:id" element={<AuthPage />} />
-					<Route
-						path="/home"
-						element={<PrivateRoute component={<HomePage />} />}
-					/>
+					<Route path="/home" element={<HomePage />} />
 					<Route path="/home/:boardName" element={<ScreenPage />} />
 					<Route path="*" element={<NotFoundPage />} />
 				</Routes>
