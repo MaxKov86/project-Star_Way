c4b8cb00fe6895e6533a9a2e6888965564591bbf
Maks <maxkov19861411@gmail.com>
2024-06-11 15:41:10 +0300

    Added PrimeBtn

---
+++ src/components/Buttons/PrimeBtn.jsx
@@ -0,0 +1,20 @@
+import { useSelector } from 'react-redux';
+import css from './PrimeBtn.module.css';
+import { selectTheme } from '../../redux/theme/selectors';
+import clsx from 'clsx';
+
+const PrimeBtn = ({ children, onBtnClick }) => {
+	const theme = useSelector(selectTheme);
+	return (
+		<button
+			onClick={() => {
+				onBtnClick();
+			}}
+			className={clsx(css.button, css[theme])}
+		>
+			{children}
+		</button>
+	);
+};
+
+export default PrimeBtn;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,14 @@
+import Button from '../../components/Buttons/PrimeBtn';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import ScreenPage from '../ScreensPage/ScreenPage';
 const HomePage = () => {
 	return (
 		<>
 			<h1>Header</h1>
+			<Button>
+				<img src="/src/assets/icon-logo.svg" alt="logo" />
+				Send
+			</Button>
 			<Sidebar />
 			<ScreenPage />
 		</>
