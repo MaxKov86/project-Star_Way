5999820d29a714ad9ae3387db415302a8665fa2d
MaxKov86 <108182903+MaxKov86@users.noreply.github.com>
2024-06-11 10:53:46 +0300

    Update CreateCard.jsx

--- src/components/Sidebar/CreateCard/CreateCard.jsx
+++ src/components/Sidebar/CreateCard/CreateCard.jsx
@@ -1,1 +1,21 @@
+import clsx from 'clsx';
+import sprite from '../../../assets/icons.svg';
+import css from './CreateCard.module.css';
+import { useSelector } from 'react-redux';
+import { selectTheme } from '../../../redux/theme/selectors';
 
+export default function CreateCard() {
+	const theme = useSelector(selectTheme);
+
+	return (
+		<div className={clsx(css.box, css[`box_${theme}`])}>
+			<p className={clsx(css.text, css[`text_${theme}`])}>Create a new board</p>
+
+			<button className={clsx(css.btn, css[`btn_${theme}`])} type="button">
+				<svg className={clsx(css.icon, css[`btn_${theme}`])}>
+					<use href={`${sprite}#icon-plus`}></use>
+				</svg>
+			</button>
+		</div>
+	);
+}
