6fcb98aca1fd69f526d10f7f3cdf23be025c0f27
Guard84 <guardsofword@gmail.com>
2024-06-11 21:51:50 +0300

    burger

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,21 @@
 import UserInfo from './UserInfo/UserInfo';
 import css from './Header.module.css';
-import { IconButton } from '@mui/material';
 import svg from '../../assets/icons.svg';
 
 const Header = () => {
 	return (
-		<header className={css.header}>
-			<IconButton className={css.menuButton}>
-				<svg width="24" height="24" style={{ fill: 'currentColor' }}>
-					<use href={`${svg}#icon-menu-01`} />
-				</svg>
-			</IconButton>
-			<UserInfo />
-		</header>
+		<div className={css.header}>
+			<header className={css.headerLayout}>
+				<div>
+					<button className={css.burger}>
+						<svg width="24" height="24">
+							<use href={svg + '#icon-menu-01'} stroke="currentColor"></use>
+						</svg>
+					</button>
+				</div>
+				<UserInfo/>
+			</header>
+		</div>
 	);
 };
 
