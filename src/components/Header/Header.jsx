9d0163d40abd04adb0010a79dca04eb84e190f82
Guard84 <guardsofword@gmail.com>
2024-06-14 12:46:07 +0300

    changeTheme

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,47 @@
+import { useState } from 'react';
 import css from './Header.module.css';
 import MenuIcon from '@mui/icons-material/Menu';
 import UserInfo from './UserInfo/UserInfo';
+import ThemeSelector from './ThemeSelector/ThemeSelector';
+import svg from '../../assets/icons.svg';
 
 const Header = ({ isOpen, toggle }) => {
-	return (
-		<div className={css.header}>
-			{innerWidth < 1440 && (
-				<button
-					className={css.burgerBtn}
-					onClick={() => toggle(!isOpen)}
-					type="button"
-				>
-					<MenuIcon className={css.userIcon} />
-				</button>
-			)}
-			<UserInfo />
-		</div>
-	);
+    const [showModal, setShowModal] = useState(false);
+
+    const closeMenuModal = () => {
+        setShowModal(false);
+    };
+
+    const handleThemeButtonClick = () => {
+        setShowModal(!showModal);
+    };
+
+    return (
+        <div className={css.header}>
+            {window.innerWidth < 1440 && (
+                <button
+                    className={css.burgerBtn}
+                    onClick={() => toggle(!isOpen)}
+                    type="button"
+                >
+                    <MenuIcon className={css.userIcon} />
+                </button>
+            )}
+            <div className={css.rightSection}>
+                <button
+                    className={css.btnTheme}
+                    onClick={handleThemeButtonClick}
+                >
+                    Theme
+                    <svg width="16" height="16" stroke="currentColor">
+                        <use href={svg + '#icon-chevron-down'}></use>
+                    </svg>
+                </button>
+                <UserInfo />
+            </div>
+			{showModal && <ThemeSelector closeMenuModal={closeMenuModal} />}
+        </div>
+    );
 };
 
 export default Header;
