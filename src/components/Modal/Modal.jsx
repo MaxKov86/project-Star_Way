4f76533617fcab97e28f416d7faca3adf2c55c69
Ivanov David <ivanovdavid905@gmail.com>
2024-06-15 16:11:27 +0300

    fimish

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,25 +1,36 @@
 import css from './Modal.module.css';
 import Modal from 'react-modal';
-import sprite from "../../assets/icons.svg"
+import sprite from "../../assets/icons.svg";
 import clsx from 'clsx';
 import { useSelector } from 'react-redux';
 import { selectTheme } from '../../redux/theme/selectors';
+import { useState } from 'react';
 
 Modal.setAppElement("#root");
 
 const OurModal = ({ children, isOpen, closeModal, title }) => {
-
 	const theme = useSelector(selectTheme);
+	const [shouldAnimate, setShouldAnimate] = useState(false);
+
+	const handleAfterOpen = () => {
+		setShouldAnimate(true);
+	};
+
+	const handleRequestClose = () => {
+		setShouldAnimate(false);
+		setTimeout(closeModal, 300); // Duration should match the animation duration
+	};
 
 	return (
 		<Modal
-			className={clsx(css.modal, css[theme])}
-			overlayClassName={clsx(css.backdrop, css[theme])}
+			className={clsx(css.modal, css[theme], { [css.animateOut]: !shouldAnimate })}
+			overlayClassName={clsx(css.backdrop, css[theme], { [css.fadeOut]: !shouldAnimate })}
 			isOpen={isOpen}
-			onRequestClose={closeModal}
+			onAfterOpen={handleAfterOpen}
+			onRequestClose={handleRequestClose}
 			contentLabel="Modal"
 		>
-			<button className={css.closeBtn} onClick={closeModal}>
+			<button className={css.closeBtn} onClick={handleRequestClose}>
 				<svg className={clsx(css.iconBtn, css[theme])}>
 					<use href={`${sprite}#icon-x-close`}></use>
 				</svg>
@@ -31,4 +42,5 @@ const OurModal = ({ children, isOpen, closeModal, title }) => {
 		</Modal>
 	);
 };
+
 export default OurModal;
