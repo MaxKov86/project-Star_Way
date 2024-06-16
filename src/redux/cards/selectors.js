9d4d411cf4dc5f502f1b60a0f47385d318f8fac3
Maks <maxkov19861411@gmail.com>
2024-06-16 18:52:39 +0300

    Updated redux and styles for ModalWindowHelp

--- src/redux/cards/selectors.js
+++ src/redux/cards/selectors.js
@@ -2,6 +2,8 @@ import { createSelector } from '@reduxjs/toolkit';
 import { selectFilter } from '../filter/selector';
 
 export const selectCards = state => state.cards.items;
+export const selectIsLoading = state => state.cards.isLoading;
+export const selectError = state => state.cards.error;
 
 export const selectFilteredCards = createSelector(
 	[selectCards, selectFilter],
--- src/redux/columns/selectors.js
+++ src/redux/columns/selectors.js
@@ -1,1 +1,3 @@
 export const selectColumns = state => state.columns.items;
+export const selectIsLoading = state => state.columns.isLoading;
+export const selectError = state => state.columns.error;
